f101b8e00f7b526d0eb5a55f428df4f07b257f7a
Chetan Lakade <1718chez@gmail.com>
2026-04-25 00:35:21 +0530

    update portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,14 @@
 const projects = [
   {
+    title: "CircuTrade E-commerce Platform",
+    description: "Scalable e-commerce platform with real-time inventory management.",
+    tech: ["Javascript", "React", "Node.js", "MongoDB"],
+    github: "https://github.com/Chez47-byte/CircuTrade",
+  },
+  {
     title: "Predictive Analysis in Stock Market Using DP",
     description: "Analyzing an fundamental of that particular stock using different datasets and apply the machine learning algorithm, i.s LSTM.",
     tech: ["Python", "SQLite", "Django", "LSTM"],
-    github: "#",
-  },
-  {
-    title: "URL Shortener",
-    description: "Scalable URL shortening service with analytics tracking.",
-    tech: ["Node.js", "MongoDB"],
     github: "#",
   },
   {
